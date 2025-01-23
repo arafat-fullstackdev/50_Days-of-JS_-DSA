fbe911f6671f9f8ec935b19868270aaa4969b513
Arafat Patwary <farabipsk@gmail.com>
2025-01-23 11:06:02 +0600

    Array DS

---
+++ Greek/array.js
@@ -0,0 +1,11 @@
+// create an array have 5  element 
+var house =new Array(10,20,30,40,50);
+// create an Array 5 undefined element
+var house_1 = new Array(5)
+
+// create an Array have 1BHK
+var home = new Array("1BHK");
+
+console.log(house);
+console.log(house_1);
+console.log(home);
