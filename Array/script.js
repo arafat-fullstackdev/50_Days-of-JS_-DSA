af7d13c3ee83081776130562352577cafa2be59e
Arafat Patwary <farabipsk@gmail.com>
2025-03-05 09:09:26 +0600

    Use string ds

---
+++ Array/script.js
@@ -0,0 +1,12 @@
+// String Ds
+let  stringStr = 'Greek';
+
+console.log(stringStr[0]);
+console.log(stringStr.charAt(2)); //Returns the character at the specified index
+
+console.log(stringStr.slice(0,2));  //start and end
+console.log(stringStr.length);
+
+let greekBreak ='GreekForBreak';
+const ans = greekBreak.split('').reverse().join('');
+console.log(ans);
